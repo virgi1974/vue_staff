f40e9a05234ed3aefa91b59cd5de37a15d37a130
virgilio1974 <virgifp@yahoo.es>
2017-05-23 22:38:07 +0200

    Favorite model - relations - seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522203935) do
+ActiveRecord::Schema.define(version: 20170523195842) do
+
+  create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "url"
+    t.string "short_description"
+    t.text "long_description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
